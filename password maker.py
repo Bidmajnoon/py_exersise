1e0fbab6935ca302f80413fc2e3288ed2ee6ab76
Ahmad khosravi <130223226+Bidmajnoon@users.noreply.github.com>
2023-08-31 10:02:56 +0330

    Update password maker.py

--- password maker.py
+++ password maker.py
@@ -2,6 +2,9 @@ import random
 import string
 def make_pass(lenth):
     char = string.ascii_letters + string.digits + string.punctuation
-    pas =''.join(random.choice(char) for i in range(lenth)) 
-    return pas
+    password = ''
+    for i in range(lenth):
+        password += random.choice(char)
+        
+    return password
 print(make_pass(7))
